992e1664826ab974a3469a5283c19a2ea942a98d
admin <admin@example.com>
2023-04-13 06:05:17 +0100

    changes for handling method channel in android.

--- android/app/src/main/kotlin/com/example/edu_native_comm/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/edu_native_comm/MainActivity.kt
@@ -1,6 +1,37 @@
 package com.example.edu_native_comm
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.edureka.demo/messages").setMethodCallHandler { call, result ->
+
+               if(call.method == "calculate") {
+                   val a = call.argument<Double>("a")
+                   val b = call.argument<Double>("b")
+
+                   if(a != null && b != null ) {
+                       val sum = a + b
+                       result.success(sum)
+                   }
+
+                   result.error("INVALID_ARGUMENTS", "invalid arguments", null)
+
+               }
+                else
+               {
+                   result.notImplemented()
+               }
+
+         }
+
+    }
+
 }
